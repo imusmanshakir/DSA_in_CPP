672675108776408f99aab0ca08f5739347a7cd9b
Usman Shakir <imusmanshakir@gmail.com>
2026-03-08 18:57:27 +0500

    update global main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,37 +1,13 @@
 #include <iostream>
-#include <climits>
+
 using namespace std;
-
-int reverseInteger(int x)
-{
-
-    int reverse = 0;
-
-    while (x != 0)
-    {
-
-        int digit = x % 10;
-
-        // Overflow check
-        if (reverse > INT_MAX / 10 || reverse < INT_MIN / 10)
-        {
-            return 0;
-        }
-
-        reverse = reverse * 10 + digit;
-
-        x = x / 10;
-    }
-
-    return reverse;
-}
 
 int main()
 {
 
-    int x = 321;
-
-    cout << reverseInteger(x) << endl;
-
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+    }
     return 0;
-}
+};
