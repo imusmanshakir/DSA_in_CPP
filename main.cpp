48c02ffcfd5e8a658c2d84914770f6af20f257fc
Usman Shakir <imusmanshakir@gmail.com>
2026-03-11 22:12:31 +0500

    Add test cases for finding pivot to the searching folder

--- main.cpp
+++ main.cpp
@@ -3,33 +3,12 @@
 
 using namespace std;
 
-int binarySearch(vector<int> array, int target)
-{
-    int start = 0, end = array.size() - 1;
-    while (start <= end)
-    {
-        int mid = (start + end) / 2;
-
-        if (target > array[mid])
-        {
-            start = mid + 1;
-        }
-        else if (target < array[mid])
-        {
-            start = mid - 1;
-        }
-        else
-        {
-            return mid;
-        }
-    }
-    return -1;
-};
 int main()
 {
-    vector<int> array = {0, 12, 3, 4, 5, 6, 7, 8, 9};
-    int target = 7;
+    vector<int> arr = {4, 5, 6, 7, 1, 2, 3};
+    int start = 0;
+    int end = arr.size() - 1;
 
-    cout << binarySearch(array, target) << endl;
-    return 0;
+    int mid = start + (end - start) / 2;
+    cout << mid << " " << endl;
 };
