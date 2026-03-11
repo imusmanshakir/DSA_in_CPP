a738990b8751331b6284625f0eeb3dc518b06768
Usman Shakir <imusmanshakir@gmail.com>
2026-03-11 22:21:03 +0500

    Add test cases for finding pivot to the searching folder

--- 08_Searching/find_pivot_in_sorted_array.cpp
+++ 08_Searching/find_pivot_in_sorted_array.cpp
@@ -2,22 +2,22 @@
 #include <vector>
 using namespace std;
 
-int findPivot(vector<int>& arr)
+int findPivot(vector<int> &arr)
 {
     int start = 0;
     int end = arr.size() - 1;
 
-    while(start <= end)
+    while (start <= end)
     {
-        int mid = start + (end - start);   // 2
+        int mid = start + (end - start) / 2;
 
-        if(mid < end && arr[mid] > arr[mid+1])
+        if (mid < end && arr[mid] > arr[mid + 1])
             return mid;
 
-        if(mid > start && arr[mid] < arr[mid-1])
+        if (mid > start && arr[mid] < arr[mid - 1])
             return mid - 1;
 
-        if(arr[start] >= arr[mid])
+        if (arr[start] >= arr[mid])
             end = mid - 1;
         else
             start = mid + 1;
@@ -28,7 +28,10 @@ int findPivot(vector<int>& arr)
 
 int main()
 {
-    vector<int> arr = {4,5,6,7,1,2,3};
+    // vector<int> arr = {10, 11, 12, 13, 1, 2, 3};   // for condition 1
+    // vector<int> arr = {6, 7, 8, 9, 1, 2, 3, 4, 5}; // for condition 2
+    // vector<int> arr = {8, 9, 1, 2, 3, 4, 5, 6, 7}; // for condition 3
+    vector<int> arr = {4, 5, 6, 7, 8, 1, 2}; // for condition 4
 
     int pivot = findPivot(arr);
 
