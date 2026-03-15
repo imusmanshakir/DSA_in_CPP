3b92fc61a62efb0cb5b53508a75b7334953f086d
Usman Shakir <imusmanshakir@gmail.com>
2026-03-15 19:33:11 +0500

    update global main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 using namespace std;
 
+int findSquare(int n)
+{
+    int start = 0;
+    int ans = 0;
+    int end = n;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        long long square = (long long)mid * mid;
+        if (square == n)
+        {
+            return mid;
+        }
+        else if (square < n)
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return ans;
+};
+
 int main()
 {
-    for (int i = 3; i >= 1; --i)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
+    int n = 20;
+    cout << findSquare(n) << " " << endl;
+
     return 0;
-}
+};
