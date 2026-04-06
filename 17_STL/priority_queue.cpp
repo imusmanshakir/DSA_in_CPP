b4cf6b2e226c80f94300bd09df6b6d04d2f58b81
Usman Shakir <imusmanshakir@gmail.com>
2026-04-06 12:28:05 +0500

    Add files to STL folder

---
+++ 17_STL/priority_queue.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    priority_queue<int, vector<int>, less<int>> maxHeap;
+
+    minHeap.push(100);
+    minHeap.push(200);
+    minHeap.push(300);
+    maxHeap.push(100);
+    maxHeap.push(200);
+    maxHeap.push(300);
+
+    cout << "Min element: " << minHeap.top() << endl;
+
+    minHeap.pop();
+
+    cout << "After poping now Min element is: " << minHeap.top() << endl;
+    cout << "Max element: " << maxHeap.top() << endl;
+
+    maxHeap.pop();
+
+    cout << "After poping now Max element is: " << maxHeap.top() << endl;
+
+    return 0;
+}
