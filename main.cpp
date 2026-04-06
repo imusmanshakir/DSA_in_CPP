b5eac7426461467aac42cafe3a5c5114e37f8279
Usman Shakir <imusmanshakir@gmail.com>
2026-04-06 12:27:37 +0500

    Update global main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
 int main()
 {
-    vector<int> arr = {1, 2, 3, 4, 5};
-    if (binary_search(arr.begin(), arr.end(), 3))
-    {
-        cout << "Foind" << " ";
-    }
-    else
-    {
-        cout << "Not found";
-    }
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    priority_queue<int, vector<int>, less<int>> maxHeap;
+
+    minHeap.push(100);
+    minHeap.push(200);
+    minHeap.push(300);
+    maxHeap.push(100);
+    maxHeap.push(200);
+    maxHeap.push(300);
+
+    cout << "Min element: " << minHeap.top() << endl;
+
+    minHeap.pop();
+
+    cout << "After poping now Min element is: " << minHeap.top() << endl;
+    cout << "Max element: " << maxHeap.top() << endl;
+
+    maxHeap.pop();
+
+    cout << "After poping now Max element is: " << maxHeap.top() << endl;
 
     return 0;
 }
