f2aa69f2091772b91020681aceaf9dc6b97523fb
Usman Shakir <imusmanshakir@gmail.com>
2026-04-17 12:03:24 +0500

    Update Algorithm folder

--- Algorithms/euclids_algo.cpp
+++ Algorithms/euclids_algo.cpp
@@ -11,6 +11,32 @@ int gcd(int a, int b)
     }
     return a;
 }
+// simple alternative formula
+int gcd1(int a, int b)
+{
+    if (a == 0)
+    {
+        return b;
+    }
+    else if (b == 0)
+    {
+        return b;
+    }
+
+    while (a != b)
+    {
+        if (a > b)
+        {
+            a = a - b;
+        }
+        else if (b > a)
+        {
+            b = b - a;
+        }
+    }
+
+    return a;
+}
 
 int main()
 {
@@ -19,6 +45,7 @@ int main()
     cin >> a >> b;
 
     cout << "GCD = " << gcd(a, b) << endl;
+    cout << "GCD1 = " << gcd1(a, b) << endl;
 
     return 0;
 }
