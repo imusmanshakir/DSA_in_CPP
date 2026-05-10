a256c80e7501fb44c8c70f853c9a5b85dc2ab252
Usman Shakir <imusmanshakir@gmail.com>
2026-05-10 21:11:15 +0500

    Add files to Linked List/Probelms subfolder

--- 04_Linked_List/Problems/0_reverse_LL_.cpp
+++ 04_Linked_List/Problems/0_reverse_LL_.cpp
@@ -66,3 +66,20 @@ int main()
 
     return 0;
 }
+
+/* you remove head = prev;, here's what will print and why:
+
+10 -> 20 -> 30 -> 40 -> 50 -> NULL
+Reversing...
+10 -> NULL
+Why?
+Even though the nodes are reversed, head in main() still points to the old first node (10).
+| Step | `curr` | Action               | Result    |
+| ---- | ------ | -------------------- | --------- |
+| 1    | 10     | `10->next = nullptr` | 10 → NULL |
+| 2    | 20     | `20->next = 10`      | 20 → 10   |
+| 3    | 30     | `30->next = 20`      | 30 → 20   |
+| 4    | 40     | `40->next = 30`      | 40 → 30   |
+| 5    | 50     | `50->next = 40`      | 50 → 40   |
+
+*/
