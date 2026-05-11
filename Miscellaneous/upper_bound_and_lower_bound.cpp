dc3d16c838c6b473fb4ee84c219b37b34abf5a9b
Usman Shakir <imusmanshakir@gmail.com>
2026-05-11 10:26:49 +0500

    add miscellaneous problems and notes

---
+++ Miscellaneous/upper_bound_and_lower_bound.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+    set<int> s = {1, 3, 5, 7};
+
+    auto lb = s.lower_bound(4);
+    auto ub = s.upper_bound(4);
+
+    cout << "Lower Bound of 4: " << *lb << endl;
+    cout << "Upper Bound of 4: " << *ub << endl;
+
+    return 0;
+}
+// lower bound checks if value exist in array if so then it return if value does not exist then it return value greater than value we wants to find.
+// More Precisely:
+// 🔹 Lower Bound
+
+// Returns an iterator to the first element ≥ value, whether the value exists or not.
+
+// ✔ If value exists → returns first occurrence
+// ✔ If value does NOT exist → returns next greater element
+
+// 🔹 Upper Bound
+
+// Returns an iterator to the first element > value
+
+// ✔ Always returns strictly greater element
+// ✔ Skips all occurrences of the value
+// ✔ upper bound simply returns greater value than the value we are looking for
